83db9a07e496bf72aaa068549e2f55dd1556f1b5
Nasandratra Sartin Harivonjy Niaina <nharivon@c1r5s8.42antananarivo.mg>
2026-07-25 17:10:29 +0300

    fix

--- xvow/lib/features/auth/presentation/sign_up_screen.dart
+++ xvow/lib/features/auth/presentation/sign_up_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' show User;
 
 import '../../../core/services/auth_service.dart';
 import '../../../shared/widgets/xvow_widgets.dart';
