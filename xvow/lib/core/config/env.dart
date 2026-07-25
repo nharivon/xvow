a7e1c525e230cfcbc2ed00ec6ef738795c2b3491
nharivon <nharivon@student.42antananarivo.mg>
2026-07-25 13:28:51 +0000

    second commit

--- xvow/lib/core/config/env.dart
+++ xvow/lib/core/config/env.dart
@@ -11,11 +11,19 @@ class Env {
 		return value;
 	}
 
-	static String get supabasePublishableKey {
-		final value = dotenv.env['SUPABASE_PUBLISHABLE_KEY'];
+	static String get baseUrl {
+		final value = dotenv.env['BASE_URL'];
 		if (value == null || value.isEmpty) {
-			throw StateError('SUPABASE_ANONKEY is not set in .env');
+			throw StateError('BASE_URL is not set in .env');
 		}
 		return value;
+	}
+
+  static String get supabasePublishableKey {
+    final value = dotenv.env['SUPABASE_PUBLISHABLE_KEY'];
+    if (value == null || value.isEmpty) {
+      throw StateError('SUPABASE_ANONKEY is not set in .env');
+    }
+    return value;
 	}
 }
